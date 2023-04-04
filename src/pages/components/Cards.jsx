828c2178c598bc5fadfa9c20554682bc2423a1b9
Anfal79 <anfalibrahim622@gmail.com>
2023-04-04 23:52:32 +0700

    adding component

--- src/pages/components/Cards.jsx
+++ src/pages/components/Cards.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import Image from 'next/image'
 
-const Cards = () => {
+const Cards = ({data}) => {
   return (
     <div>
-          <div className="card card-compact w-96 bg-base-100 shadow-xl">
-              <figure><Image src="" alt="Shoes" /></figure>
+          <div className="card card-compact glass shadow-xl text-center cursor-pointer">
+            <div className='flex flex-col items-center'>
+              <Image src={data.image} className='w-56 p-4' alt="language" />
               <div className="card-body">
-                  <h2 className="card-title">Shoes!</h2>
-                  <p>If a dog chews shoes whose shoes does he choose?</p>
-                  <div className="card-actions justify-end">
-                      <button className="btn btn-primary">Buy Now</button>
-                  </div>
+                  <h2 className="card-title">{data.name}</h2>
               </div>
+
+            </div>
           </div>
     </div>
   )
